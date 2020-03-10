05ec8b29aae5f8395016216b3bd422b9232f1a3d
BrSeal <br_se@mail.ru>
2020-03-11 01:08:03 +0300

    9.5 Домашняя работа
    добавлен psvm и поправлено исключение с пустым или null путем (при генерации исключения поле конструктора input теперь не пустое)

--- src/main/java/module09/task01/SizeCalculator/SizeCalculator.java
+++ src/main/java/module09/task01/SizeCalculator/SizeCalculator.java
@@ -1,8 +1,11 @@
 package module09.task01.SizeCalculator;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 import java.nio.file.InvalidPathException;
+import java.util.Objects;
 
 
 public class SizeCalculator {
@@ -10,10 +13,34 @@ public class SizeCalculator {
     public static final long BYTES_IN_MB = 1048576;
     public static final long BYTES_IN_GB = 1099511627776L;
 
+    public static final String helloMsg="Insert path to file or directory you want to measure or \"exit\" to quit:";
+    public static final String errMsg="Enter path to existing file or directory!";
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        while (true) {
+            try {
+                System.out.println(helloMsg);
+                input = reader.readLine();
+                if (input.equalsIgnoreCase("exit")) break;
+
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found!");
+                System.out.println(errMsg);
+            } catch (InvalidPathException e) {
+                System.out.println("Path is "+e.getInput()+"!");
+                System.out.println(errMsg);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static long calculate(String path) throws FileNotFoundException {
-        if(path==null) throw new InvalidPathException("","Path is null!", -1);
-        if(path.isEmpty()) throw new InvalidPathException("","Path is empty!", -1);
+        if (path == null) throw new InvalidPathException("null", "Path is null!", -1);
+        if (path.isEmpty()) throw new InvalidPathException("empty", "Path is empty!", -1);
 
         File file = new File(path);
         if (!file.exists()) throw new FileNotFoundException();
@@ -30,17 +57,17 @@ public class SizeCalculator {
     }
 
     public static String formatSize(long size) {
-        StringBuilder sb=new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-        if((size/BYTES_IN_GB)>0) sb.append(size/BYTES_IN_GB).append(" Gb ");
-        size%=BYTES_IN_GB;
+        if ((size / BYTES_IN_GB) > 0) sb.append(size / BYTES_IN_GB).append(" Gb ");
+        size %= BYTES_IN_GB;
 
-        if((size/BYTES_IN_MB)>0) sb.append(size/BYTES_IN_MB).append(" Mb ");
-        size%=BYTES_IN_MB;
+        if ((size / BYTES_IN_MB) > 0) sb.append(size / BYTES_IN_MB).append(" Mb ");
+        size %= BYTES_IN_MB;
 
-        if((size/BYTES_IN_KB)>0) sb.append(size/BYTES_IN_KB).append(" Kb ");
-        if(size%BYTES_IN_KB!=0) sb.append(size%BYTES_IN_KB).append(" b");
+        if ((size / BYTES_IN_KB) > 0) sb.append(size / BYTES_IN_KB).append(" Kb ");
+        if (size % BYTES_IN_KB != 0) sb.append(size % BYTES_IN_KB).append(" b");
 
-        return sb.length()==0?"0 bytes":sb.toString().trim();
+        return sb.length() == 0 ? "0 bytes" : sb.toString().trim();
     }
 }
