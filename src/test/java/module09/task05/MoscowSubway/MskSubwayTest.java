526142985751a5b9d84110281b0bf1a3d519efc7
BrSeal <50320817+BrSeal@users.noreply.github.com>
2020-03-18 00:11:01 +0300

    9.14 Домашняя работа
    Доделан парсинг веб-страницы

--- src/test/java/module09/task05/MoscowSubway/MskSubwayTest.java
+++ src/test/java/module09/task05/MoscowSubway/MskSubwayTest.java
@@ -1,6 +1,7 @@
 package module09.task05.MoscowSubway;
 
 import module08.task02.SBPMetro.core.Line;
+import module08.task02.SBPMetro.core.Station;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ class MskSubwayTest {
     void parse() throws IOException {
         MskSubway s = new MskSubway();
         s.parse("src/main/resources/09_FilesAndNetwork/files/MetroMsk.html");
-        for (Map.Entry<Integer, Line> e : s.getLines().entrySet()) {
-            System.out.println(e.getKey() + " " + e.getValue().getName());
-        }
+       System.out.println(s.getConnections());
+//        for (Map.Entry<Integer, Line> e : s.getLines().entrySet()) {
+//            System.out.println(e.getKey() + " " + e.getValue().getName());
+//        }
     }
 }
