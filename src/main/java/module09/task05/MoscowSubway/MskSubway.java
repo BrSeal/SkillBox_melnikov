c6e91535e4f72d50821ca2c53d72e97d3b50040a
BrSeal <br_se@mail.ru>
2020-03-18 12:05:47 +0300

    9.14 Домашняя работа
    Добавлен парсинг линий и станций.
    Исправлено дублирование переходов с линии на линию

--- src/main/java/module09/task05/MoscowSubway/MskSubway.java
+++ src/main/java/module09/task05/MoscowSubway/MskSubway.java
@@ -13,93 +13,95 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
-public class MskSubway
-{
-	
-	
-	private TreeMap<Integer, Line> lines;
-	private TreeMap<Integer, ArrayList<Station>> stationsOrdered;
-	private HashSet<TreeMap<Integer, String>> connections;
-	
-	public TreeMap<Integer, Line> getLines() {
-		return lines;
-	}
-	
-	public TreeMap<Integer, ArrayList<Station>> getStationsOrdered() {
-		return stationsOrdered;
-	}
-	
-	public HashSet<TreeMap<Integer, String>> getConnections() {
-		return connections;
-	}
-	
-	public MskSubway() {
-		lines = new TreeMap<>();
-		stationsOrdered = new TreeMap<>();
-		connections = new HashSet<>();
-	}
-	
-	public void parse(String url) throws IOException {
-		Document doc;
-        if (url.startsWith("http")) { doc = Jsoup.connect(url).get(); }
-        else {
+public class MskSubway {
+    private TreeMap<Integer, Line> lines;
+    private TreeMap<Integer, ArrayList<Station>> stationsOrdered;
+    private HashSet<TreeSet<String>> connections;
+
+    public TreeMap<Integer, Line> getLines() {
+        return lines;
+    }
+
+    public TreeMap<Integer, ArrayList<Station>> getStationsOrdered() {
+        return stationsOrdered;
+    }
+
+    public HashSet<TreeSet<String>> getConnections() {
+        return connections;
+    }
+
+    public MskSubway() {
+        lines = new TreeMap<>();
+        stationsOrdered = new TreeMap<>();
+        connections = new HashSet<>();
+    }
+
+    public void parse(String url) throws IOException {
+        Document doc;
+        if (url.startsWith("http")) {
+            doc = Jsoup.connect(url).get();
+        } else {
             File html = new File(url);
-            if (!html.exists()) { throw new FileNotFoundException(); }
+            if (!html.exists()) {
+                throw new FileNotFoundException();
+            }
             doc = Jsoup.parse(html, "UTF-8");
         }
-		
-		Elements rows = doc.select("table[class='standard sortable jquery-tablesorter']").select("tbody").select("tr");
-		
-		for (Element row : rows) {
-			Elements cols = row.select("td");
-			getData(cols);
-		}
-	}
-	
-	private void getData(Elements cols) {
-		String lineName = cols.get(0).select("span").get(1).attr("title");
-		String stationName = cols.get(1).select("a").get(0).text();
-		int num = getNum(cols);
-		
-		//line
-		Line line = new Line(num, lineName);
-		lines.put(num, line);
-		
-		//station
-		Station station = new Station(stationName, line);
-		addToStationsMap(num, station);
-		
-		//connection
-		Elements connected = cols.get(3).select("span");
-		if (connected.size()/2 > 0) {
-			TreeMap<Integer, String> localConn = new TreeMap<>();
-			localConn.put(num, stationName);
-			
-			for (int i=0;i<connected.size() ; i+=2) {
-			    int numLineConnected=Integer.parseInt(connected.get(i).text().replace('А','1'));
-                String nameConnected=purifyName(connected.get(i+1).attr("title"));
-				localConn.put(numLineConnected, nameConnected);
-			}
-			connections.add(localConn);
-		}
-	}
-	
-	private int getNum(Elements cols) {
-		return Integer.parseInt(cols.get(0).select("span").first().text().replace("А", "1"));
-	}
-	
-	private void addToStationsMap(int lineNum, Station station) {
-		if (!stationsOrdered.containsKey(lineNum)) {
-			stationsOrdered.put(lineNum, new ArrayList<>());
-		}
-		stationsOrdered.get(lineNum).add(station);
-	}
-	
-	private String purifyName(String name) {
-		int trimPlace = name.contains("Кросс") ? 41 : 19;
-		return name.substring(trimPlace).split(" \\(?[А-Я]?\\S+-? [а-я]+ ?[а-я]+\\)?")[0];
-	}
+
+        Elements rows = doc.select("table[class='standard sortable jquery-tablesorter']").select("tbody").select("tr");
+
+        for (Element row : rows) {
+            Elements cols = row.select("td");
+            getData(cols);
+        }
+    }
+
+    private void getData(Elements cols) {
+        int num = getNum(cols.get(0).select("span").get(0));
+        String lineName = cols.get(0).select("span").get(1).attr("title");
+        String stationName = cols.get(1).select("a").get(0).text();
+
+        //line
+        Line line = new Line(num, lineName);
+        lines.put(num, line);
+
+        //station
+        Station station = new Station(stationName, line);
+        addToStationsMap(num, station);
+
+        //connection
+        Elements connected = cols.get(3).select("span");
+        if (connected.size() / 2 > 0) {
+            TreeSet<String> localConn = new TreeSet<>();
+            localConn.add(num + ";" + stationName);
+
+            for (int i = 0; i < connected.size(); i += 2) {
+                int numLineConnected = getNum(connected.get(i));
+                String nameConnected = purifyName(connected.get(i + 1).attr("title"));
+                localConn.add(numLineConnected + ";" + nameConnected);
+            }
+            connections.add(localConn);
+        }
+    }
+
+    private int getNum(Element element) {
+        return Integer.parseInt(element.text()
+                .replace("А", "1"));
+    }
+
+    private void addToStationsMap(int lineNum, Station station) {
+        if (!stationsOrdered.containsKey(lineNum)) {
+            stationsOrdered.put(lineNum, new ArrayList<>());
+        }
+        stationsOrdered.get(lineNum).add(station);
+    }
+
+    private String purifyName(String name) {
+        int trimPlace = name.contains("Кросс") ? 41 : 19;
+        return name.substring(trimPlace).split(" \\(?[А-Я]?\\S+-? [а-я]+ ?[а-я]+\\)?")[0];
+    }
 }
 
 
--- src/test/java/module09/task05/MoscowSubway/MskSubwayTest.java
+++ src/test/java/module09/task05/MoscowSubway/MskSubwayTest.java
@@ -1,21 +1,15 @@
 package module09.task05.MoscowSubway;
 
-import module08.task02.SBPMetro.core.Line;
-import module08.task02.SBPMetro.core.Station;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Map;
 
 class MskSubwayTest {
-
     @Test
     void parse() throws IOException {
         MskSubway s = new MskSubway();
         s.parse("src/main/resources/09_FilesAndNetwork/files/MetroMsk.html");
-       System.out.println(s.getConnections());
-//        for (Map.Entry<Integer, Line> e : s.getLines().entrySet()) {
-//            System.out.println(e.getKey() + " " + e.getValue().getName());
-//        }
+
+        s.getConnections().forEach(System.out::println);
     }
 }
