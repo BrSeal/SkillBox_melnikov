781201c6ba86cb14f75b3e872532f0d59bce3f5b
BrSeal <br_se@mail.ru>
2020-03-25 16:36:26 +0300

    10.4 Домашняя работа №10.2.3
    Написать код, который выведет для каждого курса среднее количество покупок в месяц. Лучше только средствами SQL (вариант “со звёздочкой”), но группировку по месяцам можно реализовать и с помощью Java.
    
    Перекинул в отдельный проект

--- 10_SQLAndHibernate/JDBC-exp/src/main/java/JDBCexp.java
+++ 10_SQLAndHibernate/JDBC-exp/src/main/java/JDBCexp.java
@@ -1,28 +1,23 @@
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class JDBCexp
-{
-	private Statement statement;
-	private Connection connection;
-	private ResultSet resultSet;
-	
-	public JDBCexp(String url, String user, String password) throws SQLException {
-		connection = DriverManager.getConnection(url, user, password);
-		statement = connection.createStatement();
-	}
-	
-	public ResultSet query(String sqlQuery) throws SQLException {
-		
-		return resultSet = statement.executeQuery(sqlQuery);
-	}
-	
-	public void close() throws SQLException{
-		resultSet.close();
-		statement.close();
-		connection.close();
-	}
+public class JDBCexp {
+    private Statement statement;
+    private Connection connection;
+    private ResultSet resultSet;
+
+    public JDBCexp(String url, String user, String password) throws SQLException {
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+    }
+
+    public ResultSet query(String sqlQuery) throws SQLException {
+
+        return resultSet = statement.executeQuery(sqlQuery);
+    }
+
+    public void close() throws SQLException {
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
 }
--- 10_SQLAndHibernate/JDBC-exp/src/test/java/JDBCexpTest.java
+++ 10_SQLAndHibernate/JDBC-exp/src/test/java/JDBCexpTest.java
@@ -1,23 +1,50 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class JDBCexpTest
-{
-	String user="root";
-	String pass="root";
-	String url="jdbc:mysql://localhost:3306/skillbox?serverTimezone=UTC";
-	String query="select * from courses";
-	@Test
-	void query() throws SQLException {
-		JDBCexp db=new JDBCexp(url,user,pass);
-		ResultSet result=db.query(query);
-		while (result.next()){
-			System.out.println(result.getString("name"));
-		}
-		db.close();
-	}
+class JDBCexpTest {
+    static String user = "root";
+    static String pass = "root";
+    static String url = "jdbc:mysql://localhost:3306/skillbox?serverTimezone=UTC";
+
+    private JDBCexp db;
+    String query;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        db = new JDBCexp(url, user, pass);
+    }
+
+    @Test
+    void helloWorldQuery()  throws SQLException{
+        query = "SELECT 'hello world'";
+		ResultSet queryResult = db.query(query);
+		queryResult.next();
+		assertEquals(queryResult.getString("hello world"), "hello world");
+    }
+
+    @Test
+    void taskQuery() throws SQLException {
+		/*
+		Написать код, который выведет для каждого курса среднее количество покупок в месяц.
+		Лучше только средствами SQL (вариант “со звёздочкой”),
+		но группировку по месяцам можно реализовать и с помощью Java.
+		 */
+        query = "SELECT DISTINCT course_name," +
+                "COUNT(subscription_date)/ COUNT(MONTH( subscription_date)) AS avg_per_month\n" +
+                "FROM purchaselist\n" +
+                "GROUP BY course_name\n" +
+                "ORDER BY course_name";
+
+        ResultSet result = db.query(query);
+        while (result.next()) {
+            System.out.println(result.getString("course_name") +
+                    result.getString("avg_per_month"));
+        }
+        db.close();
+    }
 }
