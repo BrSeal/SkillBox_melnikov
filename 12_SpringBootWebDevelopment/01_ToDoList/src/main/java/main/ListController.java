797cf4bb95271da855c23fd473a9058ed40a037d
BrSeal <50320817+BrSeal@users.noreply.github.com>
2020-05-10 01:52:18 +0300

    12.14 Домашняя работа 12.14
    В приложении ToDoList реализовать добавление, вывод и удаление дел через базу данных.

--- 12_SpringBootWebDevelopment/01_ToDoList/src/main/java/main/ListController.java
+++ 12_SpringBootWebDevelopment/01_ToDoList/src/main/java/main/ListController.java
@@ -1,80 +1,108 @@
 package main;
 
+import main.model.Task;
+import main.model.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-public class ListController {
-    @GetMapping("/")
-    public List<String> getAll() {
-        return Storage.getTasks();
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getByNumber(@PathVariable int id) {
-        try {
-            return new ResponseEntity<>(Storage.getById(id), HttpStatus.OK);
-        } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-    
-    @PostMapping("/add")
-    public int addToList(String task) {
-        return Storage.add(task);
-    }
-    
-    @PatchMapping("/edit/{id}")
-    public ResponseEntity edit(@PathVariable int id, String data) {
-        try {
-            Storage.edit(id, data);
-            return ResponseEntity.status(HttpStatus.OK).body(null);
-        } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-    
-    @PatchMapping("/moveUp/{id}")
-    public ResponseEntity moveUp(@PathVariable int id) {
-        synchronized (Storage.class) {
-            if (id <= 0 || Storage.getSize() < 2) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-            }
-            swap(id, id - 1);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(null);
-    }
-    
-    @PatchMapping("/moveDown/{id}")
-    public ResponseEntity moveDown(@PathVariable int id) {
-        synchronized (Storage.class) {
-            if (id >= Storage.getSize() - 1) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-            }
-            swap(id, id + 1);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(null);
-    }
-    
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> delete(@PathVariable int id) {
-        try {
-            return new ResponseEntity<>(Storage.delete(id), HttpStatus.OK);
-        } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-    
-    private void swap(int a, int b) {
-        synchronized (Storage.class) {
-            String aValue = Storage.getById(a);
-            String bValue = Storage.getById(b);
-            Storage.edit(a, bValue);
-            Storage.edit(b, aValue);
-        }
-    }
-    
+public class ListController
+{
+	
+	@Autowired
+	private ToDoListRepository repository;
+	
+	@GetMapping ("/")
+	public List<Task> getAll() {
+		ArrayList<Task> tasks = new ArrayList<>();
+		repository.findAll().forEach(tasks::add);
+		return tasks;
+	}
+	
+	@GetMapping ("/{id}")
+	public ResponseEntity<Task> getByNumber(@PathVariable int id) {
+		Optional<Task> optionalTask = repository.findById(id);
+		return optionalTask.map(task -> new ResponseEntity<>(task, HttpStatus.OK)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+	}
+	
+	@PostMapping ("/add")
+	public int addToList(Task task) {
+		return repository.save(task).getId();
+	}
+	
+	@PatchMapping ("/edit/{id}")
+	public ResponseEntity edit(@PathVariable int id, String newData) {
+		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+		
+		Object o = new Object();
+		synchronized (o) {
+			
+			Optional<Task> optionalTask = repository.findById(id);
+			if (optionalTask.isPresent()) {
+				Task task = optionalTask.get();
+				task.setData(newData);
+				repository.save(task);
+				httpStatus = HttpStatus.OK;
+			}
+			
+		}
+		return ResponseEntity.status(httpStatus).body(null);
+	}
+	
+	@PatchMapping ("/moveUp/{id}")
+	public ResponseEntity moveUp(@PathVariable int id) {
+		return swap(id, id - 1);
+	}
+	
+	@PatchMapping ("/moveDown/{id}")
+	public ResponseEntity moveDown(@PathVariable int id) {
+		return swap(id, id + 1);
+	}
+	
+	@DeleteMapping ("/delete/{id}")
+	public ResponseEntity delete(@PathVariable int id) {
+		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+		
+		Optional<Task> optionalTask = repository.findById(id);
+		if (optionalTask.isPresent()) {
+			repository.deleteById(id);
+			httpStatus = HttpStatus.OK;
+		}
+		return ResponseEntity.status(httpStatus).body(null);
+	}
+	
+	private ResponseEntity swap(int a, int b) {
+		HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+		
+		Object o = new Object();
+		synchronized (o) {
+			Optional<Task> first = repository.findById(a);
+			Optional<Task> second = repository.findById(b);
+			
+			if (first.isPresent() && second.isPresent()) {
+				Task firstTask = first.get();
+				Task secondTask = second.get();
+				
+				String firstData = firstTask.getData();
+				
+				firstTask.setData(secondTask.getData());
+				secondTask.setData(firstData);
+				
+				repository.save(firstTask);
+				repository.save(secondTask);
+				
+				
+				httpStatus = HttpStatus.OK;
+			}
+		}
+		
+		return ResponseEntity.status(httpStatus).body(null);
+	}
+	
 }
--- 12_SpringBootWebDevelopment/01_ToDoList/src/main/java/main/model/ToDoListRepository.java
+++ 12_SpringBootWebDevelopment/01_ToDoList/src/main/java/main/model/ToDoListRepository.java
@@ -1,4 +1,8 @@
 package main.model;
 
-public interface ToDoListRepository
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoListRepository  extends CrudRepository<Task,Integer>
 {}
